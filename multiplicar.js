fdecd049a23a349a60292214422be80c7c8069f6
DavidGC28 <davicho9913@gmail.com>
2026-05-02 16:50:50 -0500

    tablas modificacion final

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,35 @@
-function generarTablas(){
-    let contenedor = document.getElementById("contenedorTabla")
-    contenedor.innerHTML = "<h1/>PROBANDO</h1>";
-    let contenido = "";
-    for(let i = 1; i <= 10; i++){
-        contenido=contenido + "<p>"+"3 x "+i+" = "+(3*i)+"</p>";
-    }
-    contenedor.innerHTML = contenido;
+function generarTablas() {
+  
+    let inputTabla = document.getElementById("txtTabla");
+    let contenedor = document.getElementById("contenedorTabla");
+    let titulo = document.getElementById("tituloTabla");
 
+  
+    let num = inputTabla.value;
+
+  
+    if (num === "") {
+        alert("Por favor, ingresa un número para generar la tabla.");
+        return;
+    }
+
+    titulo.innerText = "Tabla del " + num;
+
+    
+    let contenidoHTML = "";
+
+    for (let i = 1; i <= 10; i++) {
+        let resultado = num * i;
+
+        
+        contenidoHTML += `
+            <div class="fila">
+                <span>${num} x ${i}</span> 
+                <span>=</span> 
+                <span>${resultado}</span>
+            </div>
+        `;
+    }
+
+    contenedor.innerHTML = contenidoHTML;
 }
